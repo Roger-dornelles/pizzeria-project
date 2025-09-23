13b3649816f9212a9a6938761e58552a7c10ee2f
Roger Dornelles <rsd.santo@outlook.com>
2025-09-22 21:10:02 -0300

    refactor: refactor port render

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationExceptionPipe } from './validation-exception.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationExceptionPipe());
-  await app.listen(process.env.PORT || '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
